0b8ec06390b7024eebdda4464df3160cfc7fb7ca
Caleb John <calebjohn4@gmail.com>
2014-12-29 23:47:25 -0700

    Add the the close function to close the current window

--- enso/commands/win_tools.py
+++ enso/commands/win_tools.py
@@ -338,9 +338,7 @@ class cWindow:
         win32gui.SetForegroundWindow(self._hwnd)
 
     def Close(self):
-        '''seems to only minimize the window'''
-        win32gui.CloseWindow(self._hwnd)
-##      win32gui.DestroyWindow(self._hwnd) # gives me 'access is denied' error
+        win32gui.SendMessage(self._hwnd, win32con.WM_CLOSE, 0, 0)
 
     def Maximize(self):
         win32gui.ShowWindow(self._hwnd, win32con.SW_MAXIMIZE)
@@ -699,7 +697,13 @@ def cmd_unmaximize(ensoapi):
     """ Unmaximize window if it is maximized """
     cmd_restore(ensoapi)
 
+    
+def cmd_close(ensoapi):
+    """ Close the window currently in the foreground """
+    win = DESK.GetForegroundWindow()
+    win.Close()
 
+    
 class MinimizeAllWindows(CommandObject):
     '''Minimize all visible windows'''
 
