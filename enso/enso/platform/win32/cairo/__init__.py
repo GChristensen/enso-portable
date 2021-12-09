ee167c867a298e885e3ebb36cdebdec1be76e022
GChristensen <gchristnsn@gmail.com>
2021-12-09 11:02:19 +0400

    updated swig wrappers

--- enso/enso/platform/win32/cairo/__init__.py
+++ enso/enso/platform/win32/cairo/__init__.py
@@ -2,5 +2,5 @@ from ._cairo import *
 
 # HUMANIZED EDIT: Inserting version information manually.
 
-version = '1.15.12'
-version_info = ( 1, 15, 12, 'final', 0 )
+version = '1.16.0'
+version_info = ( 1, 16, 0, 'final', 0 )
